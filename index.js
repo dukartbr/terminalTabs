ff82cfc6ab65355bcd5acd833e96c6b6ce702a1c
Brady Dukart <dukartbrady@gmail.com>
2025-09-06 12:11:21 -0500

    init

---
+++ index.js
@@ -0,0 +1,16 @@
+const crazyTrain = require("./crazyTrain.json");
+
+// 6 strings with a large number of dashes representing beats
+const guitar = Array.from(Array(6), () => new Array(150).join("-"));
+
+const riff = guitar.map((str, index) => {
+  // map over strings and get notes that match the string number
+  return crazyTrain.reduce((acc, cur) => {
+    // get previous notes on string and add spacing
+    var first = acc.substring(0, cur.beat * 3);
+    var last = acc.substring(cur.beat * 3);
+    // apply new fret note on beat
+    return first + (cur.string == index + 1 ? cur.fret : "-") + last;
+  }, str);
+});
+console.log(riff);
