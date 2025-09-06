6a9fbfebb6902ac20b4a831dcd191e1a3d77a707
Brady Dukart <dukartbrady@gmail.com>
2025-09-06 15:45:07 -0500

    makes it kinda cool

---
+++ README.md
@@ -0,0 +1,18 @@
+## #Termina lTabs
+
+## ##Riff While you code
+
+Using the following schema, terminalTabs will place your guitar tab in the terminal
+
+```
+{
+	"name": "Song Title",
+	notes: [
+		{
+			"beat": 0,
+			"string": 3,
+			"fret": 5
+		}
+	]
+}
+```
--- index.js
+++ index.js
@@ -1,16 +1,41 @@
-const crazyTrain = require("./riffs/crazyTrain.json");
+const inquirer = require("inquirer");
+const fs = require("fs");
+const path = require("path");
+
+const tabDir = path.join(process.cwd(), "tabs");
+const files = fs.readdirSync(tabDir);
+
+// Fetch the local files
+var parsedTabs = files
+  .filter((f) => f.endsWith(".json"))
+  .map((f) => JSON.parse(fs.readFileSync(path.join(tabDir, f), "utf-8")));
 
 // 6 strings with a large number of dashes representing beats
 const guitar = Array.from(Array(6), () => Array(150).join("-"));
 
-const riff = guitar.map((str, index) => {
-  // map over strings and get notes that match the string number
-  return crazyTrain.reduce((acc, cur) => {
-    // get previous notes on string and add spacing
-    var first = acc.substring(0, cur.beat * 3);
-    var last = acc.substring(cur.beat * 3);
-    // apply new fret note on beat
-    return first + (cur.string == index + 1 ? cur.fret : "-") + last;
-  }, str);
-});
-console.log(riff);
+function generateTab(tab) {
+  const riff = guitar.map((str, index) => {
+    // map over strings and get notes that match the string number
+    return parsedTabs
+      .find((t) => t.name == tab)
+      .notes.reduce((acc, cur) => {
+        // get previous notes on string and add spacing
+        var first = acc.substring(0, cur.beat * 3);
+        var last = acc.substring(cur.beat * 3, str.length);
+        // apply new fret note on beat
+        return first + (cur.string == index + 1 ? cur.fret : "") + last;
+      }, str);
+  });
+  console.log(riff);
+}
+
+function mount() {
+  const prompt = inquirer.createPromptModule();
+  prompt({
+    message: "What song would you like to play?",
+    type: "list",
+    choices: parsedTabs.map((x) => x.name),
+  }).then((ans) => generateTab(ans[""]));
+}
+
+mount();
