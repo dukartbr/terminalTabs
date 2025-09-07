cec076a4ac40a44b7ee27b5610ae540961e93ec7
Brady Dukart <dukartbrady@gmail.com>
2025-09-07 14:24:04 -0500

    adds measures

--- index.js
+++ index.js
@@ -7,29 +7,61 @@ const path = require("path");
 const tabDir = path.join(__dirname, "tabs");
 const files = fs.readdirSync(tabDir);
 
+const tuningOptions = require("./tuningOptions.json");
+
+// console.log("tuning", tuningOptions);
+
 // Fetch the local files
-var parsedTabs = files
+const parsedTabs = files
   .filter((f) => f.endsWith(".json"))
   .map((f) => JSON.parse(fs.readFileSync(path.join(tabDir, f), "utf-8")));
 
 // 6 strings with a large number of dashes representing beats
-const guitar = Array.from(Array(6), () =>
-  Array(Math.floor(process.stdout.columns * 0.85)).join("-")
-);
+const rowLength = Math.floor(process.stdout.columns * 0.25);
+const guitarString = Array(rowLength).join("---");
 
-function generateTab(tab) {
-  const riff = guitar.map((str, index) => {
-    // map over strings and get notes that match the string number
-    return parsedTabs
-      .find((t) => t.name == tab)
-      .notes.reduce((acc, cur) => {
-        // get previous notes on string and add spacing
-        var first = acc.substring(0, cur.beat * 3);
-        var last = acc.substring(cur.beat * 3, str.length);
-        // apply new fret note on beat
-        return first + (cur.string == index + 1 ? cur.fret : "") + last;
-      }, str);
+function getStringName(str, tuning) {
+  var guitarTuning = tuningOptions.find((o) => o.key == tuning);
+  return guitarTuning.strings.find((s) => s.guitarString == str).name;
+}
+
+function generateTab(tab, tuning) {
+  if (!tuning) {
+    tuning = 0;
+  }
+  const foundTab = parsedTabs.find((t) => t.name == tab);
+
+  // find smallest beat duration, which would be the highest value in the array
+  const smallestDuration = Math.max(
+    ...foundTab.notes.map(({ duration }) => duration)
+  );
+
+  const guitar = Array.from(Array(6), () => {
+    return [...guitarString]
+      .map((x, i) => ((i + 1) % smallestDuration == 0 ? "|" : x))
+      .join("");
   });
+
+  const riff = guitar.map((str, index) => {
+    const chars = str.split("");
+    foundTab.notes
+      .filter((n) => n.string == index + 1)
+      .map((n) => {
+        const position = n.beat * 3;
+        chars[position] = " ";
+        if (n.fret.length > 1) {
+          chars[position + 1] = " ";
+        }
+
+        const frettedString = n.fret.toString();
+        frettedString.split("").map((digit, di) => {
+          chars[position + di] = digit;
+        });
+      });
+
+    return getStringName(index, tuning) + ":--" + chars.join("");
+  });
+  // Rock n roll baby
   console.log(riff);
 }
 
